f2d833d71799045c37f27405d491e7f303d4ac5a
Phurinut Waranich <phurinut.wara@gmail.com>
2026-04-19 03:24:37 +0700

    plugins: strip vim-dadbod database stack

--- lua/custom/plugins/ui/bufferline.lua
+++ lua/custom/plugins/ui/bufferline.lua
@@ -34,11 +34,6 @@ return {
               text = 'File Explorer',
               seperator = true,
             },
-            {
-              filetype = 'dbui',
-              text = 'DadBod Explorer',
-              seperator = true,
-            },
           },
           close_command = require('bufdelete').bufdelete,
           -- show_buffer_close_icons = false,
